db2501b238788b9d9d053eb314820604da00546a
EregNo1 <angela301313@gmail.com>
2023-11-10 15:37:34 +0900

    다이얼로그 수정1

---
+++ Assets/Script/FadeInOut.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+    public static FadeInOut instance;
+
+    CanvasRenderer canvasRenderer;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        canvasRenderer = GetComponent<CanvasRenderer>();
+        canvasRenderer.SetAlpha(0);
+        
+    }
+
+    // Update is called once per frame
+    
+
+    public void Transition()
+    {
+        StartCoroutine(Fade());
+    }
+
+    IEnumerator Fade()
+    {
+        for (float i =0; i <= 1; i += Time.deltaTime / 1)
+        {
+            canvasRenderer.SetAlpha(i);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+
+
+        for (float i = 1; i >= 0; i -= Time.deltaTime / 1)
+        {
+            canvasRenderer.SetAlpha(i);
+            yield return null;
+        }
+
+        
+
+    }
+}
